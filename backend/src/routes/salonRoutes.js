6df880964df1ef766a7f32ea1989f747f5895db2
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-21 22:07:43 +0530

    add appointment handling routes to salon API

--- backend/src/routes/salonRoutes.js
+++ backend/src/routes/salonRoutes.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
 import { addService, getAllSalons, registerSalon, removeService, updateService } from "../controller/salonController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import { handleAppointment, viewAppointmentsForSalon } from "../controller/appointmentController.js";
 const router = Router();
 router.get("/", (_, res) => {
   return res.status(200).json({
-    message: "Welcome to the API for a Salon Booking Website",
-  });
-});
-
-router.get("/",(_, res) => {
-    return res.status(200).json({
     message: "Welcome to the API for a Salon Booking Website",
   });
 });
@@ -18,4 +13,6 @@ router.post("/register",protect,registerSalon)
 router.post("/:id/add-service",protect,addService)
 router.delete("/:id/remove-service",protect,removeService)
 router.patch("/:salonId/:serviceId/update-service",protect,updateService)
+router.post("/appointment/handle",protect, handleAppointment);
+router.post("/appointment/view",protect, viewAppointmentsForSalon);
 export default router;
