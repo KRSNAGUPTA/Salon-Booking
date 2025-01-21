986f3b446123348262a182973ae31e1940e7bb34
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-21 22:07:52 +0530

    add appointment booking and viewing routes to user API

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { getUserProfile, loginUser, registerUser } from "../controller/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import { bookAppointment, viewAppointmentsForUser } from "../controller/appointmentController.js";
 const router = Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile",protect, getUserProfile);
+router.post("/appointment/book",protect, bookAppointment);
+router.post("/appointment/view",protect, viewAppointmentsForUser);
 router.get("/", (_, res) => {
   return res.status(200).json({
     message: "Welcome to the API for a Salon Booking Website",
