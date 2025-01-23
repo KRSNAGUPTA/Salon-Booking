d3a6dca1d5c43cc07c55b6ef51cb85c8b87ef806
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-01-23 21:46:36 +0530

    add default images for salon registration and improve search regex formatting

--- backend/src/controller/salonController.js
+++ backend/src/controller/salonController.js
@@ -22,7 +22,14 @@ export const registerSalon = async (req, res) => {
     if (existingSalon) {
       return res.status(400).json({ message: "You already own a salon" });
     }
-
+    if (!images) {
+      const randomImage = [
+        "https://img.freepik.com/free-vector/hairdresser-team-concept-illustration_114360-10661.jpg?t=st=1737646986~exp=1737650586~hmac=4912bacd48d95a3311236cf96dcd7765213015d71610ea6019d5105715008f7a&w=1060",
+        "https://img.freepik.com/free-vector/barbershop-team-concept-illustration_114360-10892.jpg?t=st=1737647156~exp=1737650756~hmac=efd32a780a1722bade285c65ede363e8a0595d9099d4b00ef054fa1a4a6b90b1&w=1060",
+        "https://img.freepik.com/free-vector/hairdresser-team-concept-illustration_114360-10901.jpg?t=st=1737647198~exp=1737650798~hmac=2e3438dacea27422c7669ee7b42ca9d31c6d0e87671c1ef6b0235bf4bbfb8bb9&w=740",
+      ];
+      images = randomImage[Math.floor(Math.random() * randomImage.length)];
+    }
     const salon = await Salon.create({
       name: salonName,
       owner: _id,
@@ -228,7 +235,7 @@ export const searchSalo = async (req, res) => {
   try {
     const { searchTerm } = req.body;
     const salons = await Salon.find({
-      name: new RegExp(searchTerm, 'i')
+      name: new RegExp(searchTerm, "i"),
     });
     res.status(200).json({
       message: "Salons fetched successfully",
